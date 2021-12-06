4ceb8c01c4c1846abe9b95ebf221732e09f6b08f
zahur <zahurmeerun@hotmail.com>
2021-12-06 20:36:21 +0400

    add AWS generic settings

--- backend/settings.py
+++ backend/settings.py
@@ -157,5 +157,31 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'public/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'django-react-todo'
+    AWS_S3_REGION_NAME = 'ap-southeast-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+
+    # Static and media files
+    # To allow django-admin collectstatic to automatically put your static files in your bucket    
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static' # store files under directory `static/` in bucket `my-app-bucket`
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media' # store files under directory `media/` in bucket `my-app-bucket`
+
+    # Override static and media URLs in production
+    # STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    # MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
+    # https://django-react-todo.s3.amazonaws.com/media/death.jpeg
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
