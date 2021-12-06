22cb64661f23580454d3107ae13c4979e0cfa8f9
zahur <zahurmeerun@hotmail.com>
2021-12-06 14:13:58 +0400

    install react-boostrap plus add fetch request

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,11 @@
 import './Header.css';
 
 function App() {
-  return (
-    <div className="Header">
-        <h1>Header</h1>
-    </div>
-  );
+    return (
+        <div className="Header">
+            <h1>Header</h1>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Universe/Universe.js
+++ src/components/Universe/Universe.js
@@ -1,11 +1,35 @@
+import { React, useState, useEffect } from "react";
 import './Universe.css';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 
 function Universe() {
-  return (
-    <div className="Header">
-        <h1>Universe</h1>
-    </div>
-  );
-}
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch("/universe").then((res) => res.json())
+        .then((data) => setData(data)).catch((error) => {
+            console.log(error);
+        });        
+    }, [])
+
+    const listRequest = () => {
+        (data || []).map(element=>{
+            <Row key={element.id}>
+                <Col>{element.name}</Col>
+                <Col>{element.age}</Col>
+                <Col>{element.constellation}</Col>
+                <Col>{element.image}</Col>
+            </Row>
+        })
+    }
+    return (
+        <div className="Header">
+            <h1>Universe</h1>
+            {listRequest}
+        </div>
+    );
+    }
 
 export default Universe;
